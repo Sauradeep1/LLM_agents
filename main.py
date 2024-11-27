c1b638d87c067e04e70835d5462065069936d0ac
Shadow Monarch <saurad44@gmail.com>
2024-11-27 21:51:01 +0530

    errors

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import tool
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.agents import create_react_agent
+from typing import List, Sequence
 import os
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
 from dotenv import load_dotenv
 load_dotenv()
 from dotenv import load_dotenv, find_dotenv
@@ -49,3 +52,48 @@ os.environ["OPENAI_API_KEY"] = 'sk-proj-5Iterc2q8GLtOpF8nByv13FGXtO4aH59YDIuOIcQ
 llm = ChatOpenAI()
 generate_chain = generation_prompt | llm
 reflect_chain = reflection_prompt | llm
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: Sequence[BaseMessage]):
+    return generate_chain.invoke({"messages": state})
+
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflect_chain.invoke({"messages": messages})
+    return [HumanMessage(content=res.content)]
+
+
+builder = MessageGraph()
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+
+def should_continue(state: List[BaseMessage]):
+    if len(state) > 6:
+        return END
+    return REFLECT
+
+
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+graph.get_graph().print_ascii()
+
+if __name__ == "__main__":
+    print("Hello LangGraph")
+    inputs = HumanMessage(content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """)
+    response = graph.invoke(inputs)
